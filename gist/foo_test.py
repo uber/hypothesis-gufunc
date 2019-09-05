81a36cf1e25993e6e1af33933d0bd7fd6bc90ef6
Ryan Turner <ryan.turner@uber.com>
2019-09-05 13:22:57 -0700

    adding 2nd snippet from blog

---
+++ gist/foo_test.py
@@ -0,0 +1,16 @@
+from hypothesis import given
+from hypothesis.strategies import floats, integers
+
+
+def foo(x: int, y: float):
+    return x * y
+
+
+@given(integers(min_value=-10, max_value=10), floats(min_value=-10, max_value=10))
+def test_foo_rescale(x: int, y: float):
+    z = foo(x, y)
+    z2 = foo(2 * x, y)
+    assert 2 * z == z2
+
+
+test_foo_rescale()  # Call the test without specifying inputs!
