cfa994e500b6fed902892cbc08e083a5eb338ff2
Ryan Turner <ryan.turner@uber.com>
2019-09-05 13:15:59 -0700

    adding first snippet from blog

---
+++ gist/golden_test.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2019 Uber Technologies, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import numpy as np
+
+
+def test_np_dot_golden():
+    X = np.array([[1.5, 2], [3, 4]])
+    Y = np.array([[5, 6], [7, 8.8]])
+    Z = np.dot(X, Y)
+    assert np.all(Z == np.array([[21.5, 26.6], [43.0, 53.2]]))
